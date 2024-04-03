0f40d7e776fc2e76ec0d40f14d98e5fd88ce5df3
quinnasher <pauldivineagionoja@yahoo.com>
2024-04-03 14:03:49 +0100

    finished week 04 assignment

---
+++ scripts/w04-task.js
@@ -0,0 +1,49 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+const myProfile = {
+  name: "Divine Paul",
+  photo: "../images/my-profile-pic.jpg",
+  favoriteFoods: ["Jollof Rice", "Fried Rice", "white Rice and Stew", "Masa"],
+  hobbies: ["Programming", "Coding", "Music"],
+  placesLived: [],
+};
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+  { place: "Bauchi, Nigeria", length: "13 years" },
+  { place: "Lagos, Nigeria", length: "4 years" },
+  { place: "Jos, Nigeria", length: "2 years" },
+);
+/* DOM Manipulation - Output */
+/* Name */
+document.querySelector("#name").textContent = myProfile.name;
+
+/* Photo with attributes */
+const photoEl = document.querySelector("#photo");
+photoEl.setAttribute("src", myProfile.photo);
+photoEl.setAttribute("alt", myProfile.name);
+
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach((favFood) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = favFood;
+  document.querySelector("#favorite-foods").appendChild(liEl);
+});
+/* Hobbies List */
+myProfile.hobbies.forEach((hobby) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = hobby;
+  document.querySelector("#hobbies").appendChild(liEl);
+});
+
+/* Places Lived DataList */
+myProfile.placesLived.forEach((place) => {
+  const dtEl = document.createElement("dt");
+  const ddEl = document.createElement("dd");
+  dtEl.textContent = place.place;
+  ddEl.textContent = place.length;
+
+  document.querySelector("#places-lived").appendChild(dtEl).appendChild(ddEl);
+});
